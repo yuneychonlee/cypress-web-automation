60af3715fbfa2c0aa5dc19deb53671f889f4c663
Yuney <y.chonlee@gmail.com>
2024-06-08 19:32:54 -0700

    add cypress configs

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://www.automationexercise.com/',
+    viewportHeight: 550,
+    viewportWidth: 660,
+    experimentalStudio: true
   },
 });
